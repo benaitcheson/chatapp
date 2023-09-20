d2846aa344be07d12154f9a718283a73a7414143
Ben Aitcheson <benaitcheson@hotmail.com>
2023-09-21 07:34:31 +1000

    ADD: messages MVC

---
+++ app/models/message.rb
@@ -0,0 +1,6 @@
+class Message < ApplicationRecord
+  after_create_commit { broadcast_append_to self.room }
+  
+  belongs_to :user
+  belongs_to :room
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resources :rooms
+  resources :rooms do
+    resources :messages
+  end
+
   resources :users
+
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
+
   root 'rooms#index'
 end
