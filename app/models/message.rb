02db96ca40984fdee400eedbd3fc138a4afd7ea4
Ben Aitcheson <benaitcheson@hotmail.com>
2023-09-24 20:11:48 +1000

    ADD: messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,14 @@
 class Message < ApplicationRecord
-  after_create_commit { broadcast_append_to self.room }
-  
+  after_create_commit { broadcast_append_to room }
+  before_create :confirm_participant
+
   belongs_to :user
   belongs_to :room
+
+  def confirm_participant
+    return unless room.is_private?
+
+    is_participant = Participant.where(user_id: user.id, room_id: room.id)
+    throw :abort unless is_participant
+  end
 end
